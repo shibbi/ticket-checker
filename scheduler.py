cb91cc2afcf942a278c9c57de30961148456e80d
shibbi <shibo.fang@gmail.com>
2017-05-17 13:56:16 -0700

    Change scheduler to run cron job every hour at :01

--- scheduler.py
+++ scheduler.py
@@ -1,13 +1,15 @@
 import scraper
 
 from apscheduler.schedulers.blocking import BlockingScheduler
-
-sched = BlockingScheduler()
+from apscheduler.triggers.interval import IntervalTrigger
 
 def job():
-  print('This scraper is run every 10 minutes.')
+  print('This scraper is run every hour on the dot.')
   scraper.Scraper()
 
-sched.add_job(job, 'interval', minutes=10)
-
+sched = BlockingScheduler()
+sched.add_job(job, 'cron', minute=1, id='scraper', name='Scrape tickets every hour')
 sched.start()
+
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
